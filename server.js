93de8beacdc54ff45c0e20329dc6b03fce726c4a
LindsayLew <lmschwehr@gmail.com>
2022-07-23 12:32:28 -0700

    adding server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleware for find static assets
+app.use(express.static("public"));
+
+// middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const allRoutes = require("./controllers")
+app.use(allRoutes);
+
+
+// the file path for the index.html may not be right, watch that for later errors
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log("listenin to port " + PORT);
+})
+
+// http://localhost:PORT

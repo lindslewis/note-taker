87e396d0c7e5d267044c003c29dd69af72ca2383
LindsayLew <lmschwehr@gmail.com>
2022-07-23 15:13:31 -0700

    got notes stuff to work

--- server.js
+++ server.js
@@ -23,12 +23,16 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/", (req, res) => {
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get('/api/notes', (req, res) =>
 res.json(noteData))
+
+app.post('/api/notes', (req, res) =>
+res.json(noteData))
+
 
 app.listen(PORT, () => {
     console.log("listenin to port " + PORT);
